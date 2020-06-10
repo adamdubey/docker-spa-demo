3d0e7371b5964ccc125c16b486055159191000dd
Adam Dubey <adubey0311@gmail.com>
2020-06-10 16:06:06 -0500

    [AD][0000000] Updating src/App

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello, World!</p>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          I'm a React.js App, living in a Docker Container on an AWS EC2/EB
+          instance,
+          <br></br>
+          which was brought to you by triggering a merge to Master that notified
+          <br></br>
+          Travis CI to perform tests and deploy this app to Production.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/adamdubey/docker-spa-demo"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Project Repo
         </a>
       </header>
     </div>
--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<App />, div);
 });
 
-test('renders learn react link', () => {
+test("renders project repo link", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/project repo/i);
   expect(linkElement).toBeInTheDocument();
 });
